e5d6fa13dc6f14017c522b50aac5cc1d2952092e
Gaffar Mahmud <dev@gaffarmahmud.com>
2026-03-07 14:04:27 +0600

    Re-constructed KPISection with memory-leak fix

--- src/components/sections/KPISection.jsx
+++ src/components/sections/KPISection.jsx
@@ -1,12 +1,71 @@
+import { useEffect, useState } from "react";
 import KPIBox from "../cards/KPIBox";
+import CoinCard from "../cards/CoinCard";
+import { getTrendingCoins, getTopGainers, getPopularFour } from "../../services/coingecko"
 
 export default function KPISection() {
+
+  const [trending, setTrending] = useState([]);
+  const [gainers, setGainers] = useState([]);
+  const [popular, setPopular] = useState([]);
+
+  async function fetchData() {
+    try {
+      const [trendingData, gainersData, popularData] = await Promise.all([
+        getTrendingCoins(),
+        getTopGainers(),
+        getPopularFour()
+      ]);
+
+      setTrending(trendingData || []);
+      setGainers(gainersData || []);
+      setPopular(popularData || []);
+
+    } catch (error) {
+      console.error("Failed to load data", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 60000);
+
+    // cleanup
+    return () => {
+      clearInterval(intervalId);
+    }
+
+  }, []);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
-      <KPIBox title="Total Balance" value="$124,500.00" change={5.2} />
-      <KPIBox title="24h Profit" value="+$3,240.50" change={2.1} />
-      <KPIBox title="Active Assets" value="12" />
-      <KPIBox title="Win Rate" value="68%" change={-1.4} />
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 m-3">
+
+      {/* LEFT: 4 Popular Coins*/}
+      <div className="grid grid-cols-2 gap-3 flex-1">
+        {popular.map((coin) => {
+          return (
+          <CoinCard 
+            key={coin.id}
+            coin={coin}
+            chartData={coin.sparkline_in_7d?.price} />
+          );
+        })}
+      </div>
+
+      {/* MIDDLE: Trending KPIBox */}
+        <KPIBox
+          title="Trending"
+          icon="🔥"
+          coins={trending}
+        />
+
+        {/* RIGHT: Top Gainers KPIBox */}
+        <KPIBox
+          title="Top Gainers"
+          icon="🚀"
+          coins={gainers}
+        />
     </div>
   );
 }
